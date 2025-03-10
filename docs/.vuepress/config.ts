897780270c72996b9d2ee6ffbc104c527a827bc7
Roor Li <1563856124@qq.com>
2025-03-10 16:57:50 +0800

    前端按钮.md &background filter & new domain

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -23,7 +23,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    hostname: 'https://roorli.netlify.app/',
+    hostname: 'https://roorli.top/',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
